1c42b6dcccf685ed114db3c5a52ee206c96d35ff
jamesmitchell81 <james.mitchell81@live.co.uk>
2016-03-20 21:40:02 +0000

    reorganisation

--- app/js/main.js
+++ app/js/main.js
@@ -1,57 +1,48 @@
-Vue.component('fm-input-el', {
-  props: ['input', 'index', 'value'],
-  template: '#fm-input-el',
-  methods: {
-    upcount: function(index) {
-      this.$dispatch('count', index);
-    },
-    getValue: function() {
-      console.log(this.value);
-    }
-  }
-});
+// Vue.component('fm-input-el', {
+//   props: ['input', 'index', 'value'],
+//   template: '#fm-input-el',
+//   methods: {
+//     upcount: function(index) {
+//       this.$dispatch('count', index);
+//     },
+//     getValue: function() {
+//       console.log(this.value);
+//     }
+//   }
+// });
 
-new Vue({
-  el: '#form1',
+// new Vue({
+//   el: '#form1',
 
-  data: {
-    inputs: [
-        { value: "", id: "first-name", name: "first-name", label: "First Name", placeholder: "a", msg: 0},
-        { value: "", id: "last-name",  name: "last-name",  label: "Surname",    placeholder: "b", msg: 0}
-      ]
-  },
-  methods: {
-    submit: function() {
-      // var options = {
-      //     headers: {
-      //      "Content-Type": "application/xml"
-      //   }
-      // }
-      // var string = "<customer><first-name>" + this.inputs[0].value + "</first-name><last-name>" + this.inputs[1].value + "</last-name></customer>";
+//   data: {
+//     inputs: [
+//         { value: "", id: "first-name", name: "first-name", label: "First Name", placeholder: "a", msg: 0},
+//         { value: "", id: "last-name",  name: "last-name",  label: "Surname",    placeholder: "b", msg: 0}
+//       ]
+//   },
+//   methods: {
+//     submit: function() {
+//       var data = {
+//         "firstName": this.inputs[0].value,
+//         "lastName" : this.inputs[1].value
+//       }
 
-      // this.$http.post('http://localhost:8080/services/customers', string, options);
+//       this.$http.post('http://localhost:8080/services/customers', JSON.stringify(data))
+//       .then(function(response) {
+//         this.$http.get('http://localhost:8080/services/customers', function(customers) {
+//           console.log(customers);
 
-      var data = {
-        "firstName": this.inputs[0].value,
-        "lastName" : this.inputs[1].value
-      }
+//           this.inputs[0].value = "";
+//           this.inputs[1].value = "";
+//         });
+//       });
 
-      this.$http.post('http://localhost:8080/services/customers', JSON.stringify(data))
-      .then(function(responce) {
-        this.$http.get('http://localhost:8080/services/customers', function(customers) {
-          console.log(customers);
+//     }
+//   },
+//   events: {
+//     count: function(index) {
+//       this.inputs[index].msg++;
+//     }
+//   }
 
-          this.inputs[0].value = "";
-          this.inputs[1].value = "";
-        });
-      });
-
-    }
-  },
-  events: {
-    count: function(index) {
-      this.inputs[index].msg++;
-    }
-  }
-
-});
+// });
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 'use strict';
 
+var fs = require("fs")
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserify = require('browserify');
+var vueify = require('vueify');
 
 var sassPath = "./resource/assets/css/";
 
 gulp.task('default', function() {
   // place code for your default task here
-
+  browserify('./resource/assets/vue/main.js')
+    .transform(vueify)
+    .bundle()
+    .pipe(fs.createWriteStream("./app/js/main.js"));
 });
 
 gulp.task('sass', function () {
@@ -17,6 +23,8 @@ gulp.task('sass', function () {
              .pipe(gulp.dest('./app/css'));
 });
 
+
 gulp.task('sass:watch', function () {
   gulp.watch(sassPath + '*.sass', ['sass']);
 });
+
