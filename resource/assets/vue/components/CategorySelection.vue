566cc7933a2442edd465b1050d6000fafa57530a
jamesmitchell81 <james.mitchell81@live.co.uk>
2016-04-10 15:01:40 +0100

    setting session data

--- resource/assets/vue/components/CategorySelection.vue
+++ resource/assets/vue/components/CategorySelection.vue
@@ -1,20 +1,15 @@
 <template>
 
   <form action="">
-
-    <span class="form-element">
-      <label for="">Category</label>
-      <option-item v-for="category in categories"
-                   :type="'radio'"
-                   :name="'category'"
-                   :item="category">
-      </option-item>
-    </span>
-
-    <!-- classification selection -->
-
+      <span class="option-item" v-for="category in categories">
+        <input type="radio"
+               name="category"
+               id="{{ category.name | lowercase }}"
+               data-index="{{ category.id }}">
+        <label @click="setCategory(category.id, category.name)"
+               for="{{ category.name | lowercase }}">{{ category.name }}</label>
+      </span>
   </form>
-
 
 </template>
 
@@ -43,13 +38,20 @@
                   this.categories = [];
                   console.log(err);
                 });
+
+        console.log("cat sess", sessionStorage.getItem("category-selected"));
     },
 
-    // computed: {
-    //   categories: function() {
-
-    //     }
-    // }
+    methods: {
+      setCategory: function(index, name) {
+        var category = {
+          "id": index,
+          "name": name
+        };
+        sessionStorage.setItem("category-selected", JSON.stringify(category));
+        this.$dispatch('closeSidePanelView');
+      }
+    }
 
   }
 </script>
